55ab8529ea425e860bd4c2f15c7d11431c0cdc20
samdev2200 <samuel2200@hotmail.com>
2025-04-04 20:32:34 +0200

    Corrección naves

--- src/main/java/pruebajuego/BatallaJavaWar.java
+++ src/main/java/pruebajuego/BatallaJavaWar.java
@@ -68,41 +68,63 @@ public class BatallaJavaWar {
 
         // Simulación de la batalla entre el tanque y el barco
         try {
+            logger.info("La batalla ha comenzado entre el " + tanque.getNombre() + " y la " + barco.getNombre() + "!");
             while (tanque.getResistencia() > 0 && barco.getResistencia() > 0) {
                 // El tanque ataca primero
                 int danioTanque = tanque.atacar();
                 int defensaNave = barco.defender(danioTanque);
-        
+
                 logger.info("\n***Turno " + tanque.getNombre() + " Ataca***");
+                logger.info("Daño del tanque: " + danioTanque + " | Defensa de la nave alienígena: " + defensaNave);
+
                 if (danioTanque > defensaNave) {
-                    barco.setResistencia(barco.getResistencia() - (danioTanque - defensaNave));
-                    logger.info("\tEl " + tanque.getNombre() + " ha hecho " + (danioTanque - defensaNave) + " puntos de daño a la nave alienígena.");
-        
-                    // Verificamos si el barco ha caído
-                    if (barco.getResistencia() <= 0) {
-                        logger.info("\t¡La nave alienígena ha sido destruida! El tanque ha ganado.");
-                        break;
-                    }
+                    int danoReal = danioTanque - defensaNave;
+                    int resistenciaAntes = barco.getResistencia();
+                    barco.setResistencia(barco.getResistencia() - danoReal);
+                    int resistenciaDespues = barco.getResistencia();
+
+                    logger.info("\t¡Impacto! El " + tanque.getNombre() + " ha hecho " + danoReal + " puntos de daño a la nave alienígena.");
+                    logger.info("\tResistencia de la nave antes del ataque: " + resistenciaAntes + " | Resistencia de la nave después del ataque: " + resistenciaDespues);
                 } else {
-                    logger.info("\tEl " + tanque.getNombre() + " ha atacado pero la nave alienígena ha defendido todo el daño.");
+                    logger.info("\tEl " + tanque.getNombre() + " ha atacado, pero la nave alienígena ha defendido todo el daño.");
                 }
-        
+
+                // Verificamos si el barco ha caído
+                if (barco.getResistencia() <= 0) {
+                    logger.info("\t¡La nave alienígena ha sido destruida! El tanque ha ganado la batalla.");
+                    break;
+                }
+
                 // Ahora la nave alienígena ataca
                 int danioNave = barco.atacar();
                 int defensaTanque = tanque.defender(danioNave);
-        
+
                 logger.info("\n***Turno " + barco.getNombre() + " Ataca***");
+                logger.info("Daño de la nave alienígena: " + danioNave + " | Defensa del tanque: " + defensaTanque);
+
                 if (danioNave > defensaTanque) {
-                    tanque.setResistencia(tanque.getResistencia() - (danioNave - defensaTanque));
-                    logger.info("\tLa nave alienígena ha hecho " + (danioNave - defensaTanque) + " puntos de daño al tanque.");
-        
-                    if (tanque.getResistencia() <= 0) {
-                        logger.info("\t¡El tanque ha sido destruido! La nave alienígena ha ganado.");
-                        break;
-                    }
+                    int danoReal = danioNave - defensaTanque;
+                    int resistenciaAntes = tanque.getResistencia();
+                    tanque.setResistencia(tanque.getResistencia() - danoReal);
+                    int resistenciaDespues = tanque.getResistencia();
+
+                    logger.info("\t¡Impacto! La nave alienígena ha hecho " + danoReal + " puntos de daño al tanque.");
+                    logger.info("\tResistencia del tanque antes del ataque: " + resistenciaAntes + " | Resistencia del tanque después del ataque: " + resistenciaDespues);
                 } else {
-                    logger.info("\tLa nave alienígena ha atacado pero el tanque ha defendido todo el daño.");
+                    logger.info("\tLa nave alienígena ha atacado, pero el tanque ha defendido todo el daño.");
                 }
+
+                // Verificamos si el tanque ha caído
+                if (tanque.getResistencia() <= 0) {
+                    logger.info("\t¡El tanque ha sido destruido! La nave alienígena ha ganado la batalla.");
+                    break;
+                }
+            }
+
+            if (tanque.getResistencia() > 0 && barco.getResistencia() > 0) {
+                logger.info("La batalla ha terminado sin un ganador claro. Resistencia final -> " +
+                        tanque.getNombre() + ": " + tanque.getResistencia() +
+                        ", " + barco.getNombre() + ": " + barco.getResistencia());
             }
         } catch (Exception e) {
             logger.error("Error durante la simulación de la lucha: ", e);
@@ -121,6 +143,7 @@ public class BatallaJavaWar {
         for (int i = 0; i < numGuerreros; i++) {
             Guerreros guerrero = new Humanos("Humano" + (i + 1), "Soldado");
             listadoGuerra.add(guerrero);
+            logger.info("Guerrero creado: " + guerrero.getNombre() + " de tipo " + guerrero.getTipo());
         }
         return listadoGuerra;
     }
@@ -136,6 +159,7 @@ public class BatallaJavaWar {
         if (guerreros.size() > 10) {
             throw new EmbarcarExcepcion("El tanque no puede embarcar más de 10 humanos.");
         }
+        logger.info("Tanque creado: " + "Tanque" + " con " + guerreros.size() + " guerreros.");
         return new Tanque(1000, "Tanque", "Asalto", 8, guerreros);
     }
 
@@ -147,7 +171,10 @@ public class BatallaJavaWar {
     private NaveDestructora crearNave() {
         try {
             // Aquí podemos añadir la creación de alienígenas si es necesario
-            return new NaveDestructora(1000, "Nave de Guerra", "Acorazado", new ArrayList<>());
+            List<Guerreros> guerrerosAlienigenas = new ArrayList<>();
+            NaveDestructora nave = new NaveDestructora(1000, "Nave de Guerra", "Acorazado", guerrerosAlienigenas);
+            logger.info("Nave destructora creada: " + nave.getNombre() + " de tipo " + nave.getTipo());
+            return nave;
         } catch (Exception e) {
             logger.error("Error al crear la nave destructora.", e);
             throw new RuntimeException("Error al crear la nave destructora.", e);
